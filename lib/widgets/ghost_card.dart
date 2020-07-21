3fe90ae411ee16e73912e6155c523518c3987f36
Jorge Alcivar Hurtado Duarte <jahurtado11@utpl.edu.ec>
2020-07-20 23:52:15 -0500

    Improve ghost_card

--- lib/widgets/ghost_card.dart
+++ lib/widgets/ghost_card.dart
@@ -12,7 +12,7 @@ class GhostCard extends StatelessWidget {
     @required this.text,
     @required this.img,
     this.color,
-  }); 
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -27,36 +27,32 @@ class GhostCard extends StatelessWidget {
         child: Stack(
           alignment: AlignmentDirectional.bottomEnd,
           children: <Widget>[
-            Positioned(
-              left: 0,
-              top: 0,
-              child: Container(
-                width: 160,
-                padding: EdgeInsets.fromLTRB(16, 20, 8, 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      this.name,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: Theme.of(context).textTheme.headline6.copyWith(
-                            fontSize: 14,
-                            color: Colors.white,
-                          ),
-                    ),
-                    Text(
-                      this.text,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: Theme.of(context).textTheme.subtitle2.copyWith(
-                            fontSize: 10,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w300,
-                          ),
-                    ),
-                  ],
-                ),
+            Container(
+              width: 160,
+              padding: EdgeInsets.fromLTRB(16, 20, 16, 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    this.name,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.headline6.copyWith(
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                  ),
+                  Text(
+                    this.text,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.subtitle2.copyWith(
+                          fontSize: 10,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w300,
+                        ),
+                  ),
+                ],
               ),
             ),
             Center(
@@ -64,10 +60,7 @@ class GhostCard extends StatelessWidget {
                 height: 100,
                 width: 100,
                 margin: EdgeInsets.only(top: 16),
-                child: Hero(
-                  tag: 'ghost',
-                  child: Image.asset('assets/${this.img}.png'),
-                ),
+                child: Image.asset('assets/${this.img}.png'),
               ),
             ),
           ],
